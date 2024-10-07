9c3f6fe6a9132a7e3f06e7e4cc5206f828e97b16
User <User@DESKTOP-BC53N1O>
2024-10-07 21:14:09 +0300

    Добавьте файлы проекта.

---
+++ yoo/yoo.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+    double meters, km;
+
+    cout << "enter meters:";
+    cin >> meters;
+
+    km = meters / 1000.0;
+    cout << meters << "meters =" << km << "kilometrs" << endl;
+}
+
